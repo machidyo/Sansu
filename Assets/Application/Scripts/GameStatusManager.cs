80fc982aea3d46b6181e1bfd5029650d75ec3412
machidyo <yorihiko_m@hotmail.com>
2020-11-26 00:30:57 +0900

    add: GameStatus を司るものを追加

---
+++ Assets/Application/Scripts/GameStatusManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class GameStatusManager : MonoBehaviour
+{
+    public enum Status
+    {
+        Ready = 0,
+        CountDown = 1,
+        Playing = 2,
+        GaveOver = 3,
+    }
+
+    public ReactiveProperty<Status> CurrentStatus = new ReactiveProperty<Status>(Status.Ready);
+
+    void Start()
+    {
+        CurrentStatus.DistinctUntilChanged().Subscribe(s =>
+        {
+            switch (s)
+            {
+                case Status.Ready:
+                    break;
+                case Status.CountDown:
+                    Observable.Timer(TimeSpan.FromSeconds(4)).Subscribe(_ => CurrentStatus.Value = Status.Playing);
+                    break;
+                case Status.Playing:
+                    break;
+                case Status.GaveOver:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(s), s, null);
+            }
+        });
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CurrentStatus.Value += 1;
+        }
+    }
+}
