012c466bcc3cb975905ca4b1505b39b4e9aa3f80
machidyo <yorihiko_m@hotmail.com>
2020-11-29 20:32:23 +0900

    add: とりあえずステータスを画面に表示するようにした

---
+++ Assets/Application/Scripts/DisplayController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+public class DisplayController : MonoBehaviour
+{
+    [SerializeField] private bool isDebug;
+    
+    [SerializeField] private TextMeshProUGUI statusText;
+    
+    [SerializeField] private GameStatusManager status;
+
+    void Start()
+    {
+        if (isDebug)
+        {
+            status.CurrentStatus.DistinctUntilChanged().Subscribe(s => statusText.text = "" + s);
+        }
+        
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
