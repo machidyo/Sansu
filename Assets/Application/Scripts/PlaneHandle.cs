b01300e789808a6f971cb8829e80b16a2fd1f872
machidyo <yorihiko_m@hotmail.com>
2021-01-01 13:30:52 +0900

    add: 飛行機を動かせるようにした

---
+++ Assets/Application/Scripts/PlaneHandle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+public class PlaneHandle : MonoBehaviour
+{
+    [SerializeField] private Transform direction;
+
+    [SerializeField] private MeshRenderer renderer;
+    [SerializeField] private Material on;
+    [SerializeField] private Material off;
+
+    private bool isOn = false;
+
+    private float updateInterval = 0.01f; // s
+    private float speed = 0.01f;
+
+    void Start()
+    {
+        OnTriggerEnter(null);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction.transform.localPosition += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction.transform.localPosition += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction.transform.localPosition += new Vector3(0, speed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction.transform.localPosition += new Vector3(0, -speed, 0);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        renderer.materials = new[] {on};
+        isOn = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        renderer.materials = new[] {off};
+        isOn = false;
+    }
+}
--- Assets/Application/Scripts/PlayerPlane.cs
+++ Assets/Application/Scripts/PlayerPlane.cs
@@ -8,9 +8,15 @@ using UniRx;
 public class PlayerPlane : MonoBehaviour
 {
     private const int MAX_ROTATE_SPEED = 5;
+
+    private const float FLY_UPDATE_INTERVAL = 0.01f; // s
+    private const float FLY_SPEED = 0.01f;
     
     [SerializeField] private GameObject propeller;
-    
+    [SerializeField] private Transform direction;
+
+    private bool isReady = false;
+
     void Start()
     {
         Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x =>
@@ -19,13 +25,33 @@ public class PlayerPlane : MonoBehaviour
             var rotationSpeed = speed * 360;
             propeller
                 .transform
-                .DORotate(new Vector3(0, 0, rotationSpeed), 1f, RotateMode.FastBeyond360)
+                .DOLocalRotate(new Vector3(0, 0, rotationSpeed), 1f, RotateMode.FastBeyond360)
                 .SetEase(Ease.Linear);
+
+            if (x > 4)
+            {
+                isReady = true;
+            }
         });
+        
+        Observable.Interval(TimeSpan.FromSeconds(FLY_UPDATE_INTERVAL))
+            .Where(_ => isReady)
+            .Subscribe(_ =>
+            {
+                Fly();
+            });
     }
 
     void Update()
     {
         
     }
+    
+    private void Fly()
+    {
+        var forward = transform.rotation * new Vector3(0, 0, FLY_SPEED);
+        transform.DOMove(transform.position + forward, FLY_UPDATE_INTERVAL);
+        transform.DOLookAt(direction.position, FLY_UPDATE_INTERVAL);
+    }
+
 }
