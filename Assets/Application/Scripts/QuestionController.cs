b7dd922b63618180008c7fac278ee47dec76784a
machidyo <yorihiko_m@hotmail.com>
2021-01-04 18:48:55 +0900

    add: 問題のランダム生成と表示個所を作成

---
+++ Assets/Application/Scripts/QuestionController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
+
+public class QuestionController : MonoBehaviour
+{
+    public enum OperatorKind
+    {
+        Plus = 0,
+        Minus = 1,
+        Multiply = 2,
+        Divide = 3
+    }
+
+    public int X { get; private set; } = -1;
+    public int Y { get; private set; } = -1;
+    public OperatorKind Operation { get; private set; }
+    public int Answer { get; private set; } = -1;
+    // todo: 間違い二つ、三つと増やす
+    public int WrongAnswer { get; private set; } = -1;
+
+    void Start()
+    {
+        SetQuestion();
+    }
+
+    private void Reset()
+    {
+        X = -1;
+        Y = -1;
+        Answer = -1;
+        WrongAnswer = -1;
+    }
+    
+    public void SetQuestion()
+    {
+        Reset();
+        
+        // todo: 繰り上げ、繰り下げモードを追加する
+        while (Answer < 0)
+        {
+            X = Random.Range(0, 19);
+            Y = Random.Range(0, 19);
+            Operation = Random.Range(0, 2) % 2 == 0 ? OperatorKind.Plus : OperatorKind.Minus;
+            Answer = Calculate();
+
+            while (WrongAnswer < 0 || WrongAnswer == Answer)
+            {
+                WrongAnswer = Random.Range(0, 2) % 2 == 0 
+                    ? Answer + Random.Range(1, 3) 
+                    : Answer - Random.Range(1, 3);
+            }
+        }
+    }
+
+    public int Calculate()
+    {
+        switch (Operation)
+        {
+            case OperatorKind.Plus:
+                return X + Y;
+            case OperatorKind.Minus:
+                return X - Y;
+            case OperatorKind.Multiply:
+                return X * Y;
+            case OperatorKind.Divide:
+                return X / Y;
+            default:
+                return X + Y;
+        }
+    }
+}
---
+++ Assets/Application/Scripts/QuestionViewer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class QuestionViewer : MonoBehaviour
+{
+    [SerializeField] private GameObject questionSet;
+    
+    [SerializeField] private List<GameObject> numbers;
+    [SerializeField] private List<GameObject> operators;
+
+    private QuestionController questionController;
+
+    private void Awake()
+    {
+    }
+
+    void Start()
+    {
+        questionController = FindObjectOfType<QuestionController>();
+        ViewQuestion();
+    }
+    
+    public async void ViewQuestion()
+    {
+        await UniTask.WaitUntil(() => questionController.Answer > 0);
+        
+        var pos = transform.position + Vector3.forward * 30;
+        var set = Instantiate(questionSet, pos, Quaternion.identity);
+
+        var xTran = set.transform.FindChildRecursive("X");
+        ShowNumber(questionController.X, xTran);
+
+        var opeTran = set.transform.FindChildRecursive("Operation");
+        ShowOperator(questionController.Operation, opeTran);
+        
+        var yTran = set.transform.FindChildRecursive("Y");
+        ShowNumber(questionController.Y, yTran);
+
+        // todo: 複数の間違いに対応する
+        var leftAnswer = set.transform.FindChildRecursive("LeftAnswerNumber");
+        var rightAnswer = set.transform.FindChildRecursive("RightAnswerNumber");
+        var isEven = Random.Range(0, 2) % 2 == 0;
+        if (isEven)
+        {
+            ShowNumber(questionController.Answer, leftAnswer);
+            ShowNumber(questionController.WrongAnswer, rightAnswer);
+        }
+        else
+        {
+            ShowNumber(questionController.WrongAnswer, leftAnswer);
+            ShowNumber(questionController.Answer, rightAnswer);
+        }
+    }
+
+    /// <summary>
+    /// parent の位置をセンターにして、 number を GameObject で表示する
+    /// </summary>
+    private void ShowNumber(int number, Transform parent)
+    {
+        var indexes = new List<int>();
+        do
+        {
+            indexes.Add(number % 10);
+            number /= 10;
+        } while (number > 0);
+
+        var center = indexes.Count / 2;
+        for (var i = 0; i < indexes.Count; i++)
+        {
+            var num = Instantiate(numbers[indexes[i]], parent);
+            // 0.4f は画面上で見て良い値をマジックナンバーとして設定
+            var posX = indexes.Count % 2 == 0 
+                ? 0.4f * (center - i) - 0.2f  // 偶数個の場合センターが文字の区切りになるので -0.2f している
+                : 0.4f * (center - i);        // 奇数個の場合
+            num.transform.localPosition = new Vector3(posX, 0.0f, 0.0f);
+            num.transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    private void ShowOperator(QuestionController.OperatorKind kind, Transform parent)
+    {
+        var ope = Instantiate(operators[(int)kind], parent);
+        switch (kind)
+        {
+            case QuestionController.OperatorKind.Plus:
+                break;
+            case QuestionController.OperatorKind.Minus:
+                ope.transform.localRotation = Quaternion.Euler(0, 0, 90f);
+                ope.transform.localScale = new Vector3(1.0f, 0.8f, 1.0f);
+                break;
+            case QuestionController.OperatorKind.Multiply:
+            case QuestionController.OperatorKind.Divide:
+                throw new NotImplementedException();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(kind), kind, null);
+        }
+    }
+}
